1066890383fd02893a2e90431596984e14426fc2
Alaa <alaahero746@gmail.com>
2024-12-01 20:01:05 +0200

    Login4

---
+++ Assets/ٍScript/LoadLogin.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLogin : MonoBehaviour
+{
+   public void LoadScene()
+    {
+        SceneManager.LoadScene("Sign in");
+    }
+}
