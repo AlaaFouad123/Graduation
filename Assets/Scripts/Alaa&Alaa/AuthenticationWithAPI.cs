b9448bf9c787e0e924fdccf40f8931c88343441c
Alaa Hassan <139705110+Alaa551@users.noreply.github.com>
2025-06-11 18:09:43 +0300

    edit auth logic

--- Assets/Scripts/Alaa&Alaa/AuthenticationWithAPI.cs
+++ Assets/Scripts/Alaa&Alaa/AuthenticationWithAPI.cs
@@ -6,73 +6,72 @@ using UnityEngine.SceneManagement;
 
 public class AuthenticationWithAPI : MonoBehaviour
 {
-    public string BaseAPIURL = "https://timonoapp.runasp.net";
-    public TMP_InputField EmailInputField;
-    public TMP_InputField PasswordInputField;
-    public TMP_Text EmailError;
-    public TMP_Text PasswordError;
+	public string BaseAPIURL = "https://timonoapp.runasp.net";
+	public TMP_InputField EmailInputField;
+	public TMP_InputField PasswordInputField;
+	public TMP_Text EmailError;
+	public TMP_Text PasswordError;
 
-    public void Login()
-    {
-        var user = new UserLogin
-        {
-            email = EmailInputField.text,
-            password = PasswordInputField.text
-        };
+	public void Login()
+	{
+		var user = new UserLogin
+		{
+			email = EmailInputField.text,
+			password = PasswordInputField.text
+		};
 
-        StartCoroutine(SendLoginRequest(user));
-    }
+		StartCoroutine(SendLoginRequest(user));
+	}
 
-    public IEnumerator SendLoginRequest(UserLogin user)
-    {
-        // Convert to JSON
-        string jsonData = JsonUtility.ToJson(user);
+	public IEnumerator SendLoginRequest(UserLogin user)
+	{
+		// Convert to JSON
+		string jsonData = JsonUtility.ToJson(user);
 
-        // Create web request
-        using (UnityWebRequest request = new UnityWebRequest($"{BaseAPIURL}/Login", "POST"))
-        {
-            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
-            request.uploadHandler = new UploadHandlerRaw(jsonToSend);
-            request.downloadHandler = new DownloadHandlerBuffer();
-            request.SetRequestHeader("Content-Type", "application/json");
+		// Create web request
+		using (UnityWebRequest request = new UnityWebRequest($"{BaseAPIURL}/Login", "POST"))
+		{
+			byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
+			request.uploadHandler = new UploadHandlerRaw(jsonToSend);
+			request.downloadHandler = new DownloadHandlerBuffer();
+			request.SetRequestHeader("Content-Type", "application/json");
 
-            yield return request.SendWebRequest();
+			yield return request.SendWebRequest();
 
-            if (request.result != UnityWebRequest.Result.Success)
-            {
+			if (request.result != UnityWebRequest.Result.Success)
+			{
 
-                Debug.LogError("Login failed: " + request.downloadHandler.text);
+				Debug.LogError("Login failed: " + request.downloadHandler.text);
 
-                if (request.downloadHandler.text.Contains("email"))
-                {
-                    EmailError.text = "";
-                    EmailError.text = "Invalid email address.";
-                }
-                else if (request.downloadHandler.text.Contains("password"))
-                {
-                    PasswordError.text = "Invalid password.";
-                }
-                else
-                {
-                    EmailError.text = "Login failed. Please try again.";
-                    PasswordError.text = "";
-                }
-            }
-            else
-            {
-                Debug.Log("Login response: " + request.downloadHandler.text);
-                EmailError.text = "";
-                PasswordError.text = "";
-
-                SceneManager.LoadScene(3);
-            }
-        }
-    }
+				if (request.downloadHandler.text.Contains("email"))
+				{
+					EmailError.text = "";
+					EmailError.text = "Invalid email address.";
+				}
+				else if (request.downloadHandler.text.Contains("password"))
+				{
+					PasswordError.text = "Invalid password.";
+				}
+				else
+				{
+					EmailError.text = "Login failed. Please try again.";
+					PasswordError.text = "";
+				}
+			}
+			else
+			{
+				Debug.Log("Login response: " + request.downloadHandler.text);
+				EmailError.text = "";
+				PasswordError.text = "";
+				SceneManager.LoadScene("SkipSub");
+			}
+		}
+	}
 
 }
 
 public class UserLogin
 {
-    public string email;
-    public string password;
+	public string email;
+	public string password;
 }
